90302a72363041e155dd2e356823e4fba2cf75a1
Robin Rana <robinrana4r@gmail.com>
2025-09-26 11:49:48 +0600

    update schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,11 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
-import { RecordsModule } from './records/records.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     AuthModule,
-    UsersModule,
-    RecordsModule,
     PrismaModule,
   ],
   providers: [PrismaService, AppService],
